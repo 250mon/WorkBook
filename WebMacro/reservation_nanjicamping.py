699d5c631616a01d14808a50d97f3586e8cb4bc6
250mon <lambki@naver.com>
2021-10-16 13:46:51 +0900

    try except

--- WebMacro/reservation_nanjicamping.py
+++ WebMacro/reservation_nanjicamping.py
@@ -10,11 +10,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 from handle_ids import HandleIds
 from threading import Thread
-
+import sys
 
 class NanjiCamping(Thread):
     def __init__(self, url, date):
         Thread.__init__(self)
+        self.wait_time = 5
         self.driver = self._get_webdriver()
         self._set_driver()
         self.url = url
@@ -31,102 +32,95 @@ class NanjiCamping(Thread):
         return driver
 
     def _set_driver(self):
+        pass
         # implicit waits
-        self.driver.implicitly_wait(5)
+        # self.driver.implicitly_wait(self.wait_time)
 
     def login_from_main(self):
-        self.driver.get(url='https://yeyak.seoul.go.kr/web/loginForm.do')
-        login_btn = self.driver.find_element(By.XPATH, '//*[@id="header"]/div[1]/div/div[1]/a')
-        login_btn.click()
-        self._login_idpwd()
-        self.driver.get(self.url)
-
-    def login_from_reserv(self):
-        self.driver.get(self.url)
-        # initial popup window
-        popup = self.driver.find_element(By.XPATH, '//*[@id="contents"]/div[1]/div/div[2]/button/span')
-        popup.click()
-        # initial screen
-        reserve_btn = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[1]/div[2]/div/div/a[1]')
-        reserve_btn.click()
-        # login window
-        Alert(self.driver).accept()
-        self._login_idpwd()
+        while True:
+            self.driver.get(url='https://yeyak.seoul.go.kr/web/loginForm.do')
+            try:
+                login_btn = WebDriverWait(self.driver, self.wait_time).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="header"]/div[1]/div/div[1]/a'))
+                )
+                login_btn.click()
+                self._login_idpwd()
+            except:
+                print('Exception login from main')
+            else:
+                break
 
     def _login_idpwd(self):
         userid, userpw = HandleIds('ids.txt').get_idpw('Seoul')
-        self.driver.find_element(By.XPATH, '//*[@id="userid"]').send_keys(userid)
-        self.driver.find_element(By.XPATH, '//*[@id="userpwd"]').send_keys(userpw)
-        self.driver.find_element(By.XPATH, '//*[@id="addUserForm"]/div/button').click()
+        try:
+            userid_form = WebDriverWait(self.driver, self.wait_time).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="userid"]'))
+            )
+            userid_form.send_keys(userid)
+            self.driver.find_element(By.XPATH, '//*[@id="userpwd"]').send_keys(userpw)
+            self.driver.find_element(By.XPATH, '//*[@id="addUserForm"]/div/button').click()
+        except:
+            raise Exception
 
     def initial_screen(self):
         while True:
-            # popup window
-            popup = self.driver.find_element(By.XPATH, '//*[@id="contents"]/div[1]/div/div[2]/button/span')
-            popup.click()
-            # initail screen
-            reserve_btn = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[1]/div[2]/div/div/a[1]')
-            reserve_btn.click()
-            # if (cond := EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]/div[1]/div/a[1]'))) is True:
-            if len(dates := self.driver.find_elements(By.XPATH, f'//*[@id="calendar_2021{self.date}"]')) > 0:
-                dates[0].click()
-                break
-            else:
+            try:
+                popup = WebDriverWait(self.driver, self.wait_time).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]/div[1]/div/div[2]/button/span'))
+                )
+                popup.click()
+                # explicit waits for reservation btn
+                # initial reservation status window
+                reserve_btn = WebDriverWait(self.driver, self.wait_time).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="aform"]/div[1]/div[2]/div/div/a[1]'))
+                )
+                reserve_btn.click()
+            except:
                 self.driver.get(url=self.url)
-
-            # # explicit waits for reservation btn
-            # try:
-            #     # initial reservation status window
-            #     reserve_btn = WebDriverWait(self.driver, 5).until(
-            #         EC.presence_of_element_located((By.XPATH, '//*[@id="aform"]/div[1]/div[2]/div/div/a[1]'))
-            #     )
-            #     reserve_btn.click()
-            # finally:
-            #     if EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]/div[1]/div/a[1]')) is False:
-            #         break
-            #     else:
-            #         self.driver.get(url=self.url)
-
+            else:
+                break
 
     def macro(self):
+        # main screen for login
         self.login_from_main()
+        # after login, stand by
+        sys.stdin.read(1)
+
+        self.driver.get(self.url)
         # self.login_from_reserv()
+
+        # initial reservation screen
         self.initial_screen()
 
-        # # popup window
-        # popup = self.driver.find_element((By.XPATH, '//*[@id="contents"]/div[1]/div/div[2]/button/span')
-        # popup.click()
-        # # initial reservation status window
-        # reserve_btn = self.driver.find_element((By.XPATH, '//*[@id="aform"]/div[1]/div[2]/div/div/a[1]'))
-        # reserve_btn.click()
+        # main reservation screen
+        try:
+            select_num_nite = Select(self.driver.find_element(By.NAME, 'form_cal'))
+            all_nites = select_num_nite.all_selected_options
+            if (avail := len(all_nites)) == 0:
+                print("No availabe dates")
+                exit(0)
+            elif avail == 1:
+                index = 1
+            else:
+                index = 2
+            select_num_nite.select_by_index(index=index)
 
-        # main reservation window
-        # date = self.driver.find_element(By.XPATH, '//*[@id="calendar_20211020"]')
-        # date.click()
+            user_cnt = self.driver.find_element(By.XPATH, '//*[@id="user_cnt_area"]/div/div[1]/div/div/div/button[2]')
+            user_cnt.send_keys(Keys.ENTER)
+            usrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[5]/h5/span/label/span')
+            usrchkbox.click()
+            agrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[6]/h5/span/label/span')
+            agrchkbox.click()
+            deal_btn = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[3]/div/div[3]/button')
+            deal_btn.click()
 
-        select_num_nite = Select(self.driver.find_element(By.NAME, 'form_cal'))
-        all_nites = select_num_nite.all_selected_options
-        if (avail := len(all_nites)) == 0:
-            print("No availabe dates")
-            exit(0)
-        elif avail == 1:
-            index = 1
-        else:
-            index = 2
-        select_num_nite.select_by_index(index=index)
+            # finalize the reservation
+            Alert(self.driver).accept()
+        except:
+            print('error')
 
-        user_cnt = self.driver.find_element(By.XPATH, '//*[@id="user_cnt_area"]/div/div[1]/div/div/div/button[2]')
-        user_cnt.send_keys(Keys.ENTER)
-        usrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[5]/h5/span/label/span')
-        usrchkbox.click()
-        agrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[6]/h5/span/label/span')
-        agrchkbox.click()
-        deal_btn = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[3]/div/div[3]/button')
-        deal_btn.click()
-
-        # finalize the reservation
-        Alert(self.driver).accept()
-        sleep(5)
+        if (c := input()) == 'q':
+            pass
 
         self.driver.quit()
 
@@ -141,32 +135,17 @@ if __name__ == '__main__':
     # NanjiCamping(url=urls['10_Dtype'], date='1020').start()
 
     # Friday
-    for i in range(2):
-        NanjiCamping(url=urls['11_Dtype'], date='1126').start()
-        NanjiCamping(url=urls['11_deck'], date='1126').start()
-
-        NanjiCamping(url=urls['11_Dtype'], date='1119').start()
-        NanjiCamping(url=urls['11_deck'], date='1119').start()
-
-        NanjiCamping(url=urls['11_Dtype'], date='1112').start()
-        NanjiCamping(url=urls['11_deck'], date='1112').start()
-
-        NanjiCamping(url=urls['11_Dtype'], date='1105').start()
-        NanjiCamping(url=urls['11_deck'], date='1105').start()
-
-        sleep(2)
-
-    sleep(10)
+    NanjiCamping(url=urls['11_Dtype'], date='1105').start()
+    # NanjiCamping(url=urls['11_deck'], date='1105').start()
+    # NanjiCamping(url=urls['11_Dtype'], date='1112').start()
+    # NanjiCamping(url=urls['11_deck'], date='1112').start()
+    # NanjiCamping(url=urls['11_Dtype'], date='1119').start()
+    # NanjiCamping(url=urls['11_deck'], date='1119').start()
+    # NanjiCamping(url=urls['11_Dtype'], date='1126').start()
+    # NanjiCamping(url=urls['11_deck'], date='1126').start()
 
     # Thursday
-    NanjiCamping(url=urls['11_Dtype'], date='1124').start()
-    NanjiCamping(url=urls['11_deck'], date='1124').start()
-
-    NanjiCamping(url=urls['11_Dtype'], date='1117').start()
-    NanjiCamping(url=urls['11_deck'], date='1117').start()
-
-    NanjiCamping(url=urls['11_Dtype'], date='1110').start()
-    NanjiCamping(url=urls['11_deck'], date='1110').start()
-
-    NanjiCamping(url=urls['11_Dtype'], date='1103').start()
-    NanjiCamping(url=urls['11_deck'], date='1103').start()
+    # # NanjiCamping(url=urls['11_deck'], date='1103').start()
+    # # NanjiCamping(url=urls['11_deck'], date='1110').start()
+    # # NanjiCamping(url=urls['11_deck'], date='1117').start()
+    # NanjiCamping(url=urls['11_deck'], date='1124').start()
