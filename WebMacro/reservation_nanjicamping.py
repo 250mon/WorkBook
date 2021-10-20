671e9ec9eb92620dba89d819656ebc8a5c0844d4
250mon <lambki@naver.com>
2021-10-20 19:03:06 +0900

    almost done

--- WebMacro/reservation_nanjicamping.py
+++ WebMacro/reservation_nanjicamping.py
@@ -1,3 +1,5 @@
+import time
+
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
@@ -13,20 +15,21 @@ from threading import Thread
 import sys
 
 class NanjiCamping(Thread):
-    def __init__(self, url, date):
+    def __init__(self, url, date, type='camping'):
         Thread.__init__(self)
         self.wait_time = 5
         self.driver = self._get_webdriver()
         self._set_driver()
         self.url = url
         self.date = date
+        self.type = type
 
     def run(self):
         self.macro()
 
     def _get_webdriver(self):
         options = webdriver.ChromeOptions()
-        # options.add_argument('headless')
+        options.add_argument('headless')
         options.add_argument('window-size=1920,1080')
         driver = webdriver.Chrome(executable_path='chromedriver', options=options)
         return driver
@@ -48,6 +51,7 @@ class NanjiCamping(Thread):
             except:
                 print('Exception login from main')
             else:
+                print('Login successful')
                 break
 
     def _login_idpwd(self):
@@ -64,6 +68,7 @@ class NanjiCamping(Thread):
 
     def initial_screen(self):
         while True:
+            self.driver.get(url=self.url)
             try:
                 popup = WebDriverWait(self.driver, self.wait_time).until(
                     EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]/div[1]/div/div[2]/button/span'))
@@ -76,67 +81,91 @@ class NanjiCamping(Thread):
                 )
                 reserve_btn.click()
             except:
-                self.driver.get(url=self.url)
+                print('Initial_screen failed... reload the page')
             else:
+                print('Initial_screen successful')
                 break
 
     def macro(self):
         # main screen for login
         self.login_from_main()
         # after login, stand by
-        sys.stdin.read(1)
+        # sys.stdin.read(1)
 
-        self.driver.get(self.url)
-        # self.login_from_reserv()
+        while True:
+            # initial reservation screen
+            self.initial_screen()
 
-        # initial reservation screen
-        self.initial_screen()
+            # main reservation screen
+            try:
+                # select date
+                if len(dates := self.driver.find_elements(By.XPATH, f'//*[@id="calendar_2021{self.date}"]')) > 0:
+                    dates[0].click()
+                else:
+                    continue
 
-        # main reservation screen
-        try:
-            select_num_nite = Select(self.driver.find_element(By.NAME, 'form_cal'))
-            all_nites = select_num_nite.all_selected_options
-            if (avail := len(all_nites)) == 0:
-                print("No availabe dates")
-                exit(0)
-            elif avail == 1:
-                index = 1
+                if self.type == 'camping':
+                    # select number of nights
+                    select_num_nite = Select(self.driver.find_element(By.NAME, 'form_cal'))
+                    all_nites = select_num_nite.all_selected_options
+                    if (avail := len(all_nites)) == 0:
+                        print("No availabe dates")
+                        continue
+                    elif avail == 1:
+                        index = 1
+                    else:
+                        index = 2
+                    select_num_nite.select_by_index(index=index)
+                elif self.type == 'barbecue11':
+                    timezone = self.driver.find_element(By.XPATH, '//*[@id="unit0"]/a')
+                    timezone.click()
+                else:  #self.type == 'barbecue17':
+                    timezone = self.driver.find_element(By.XPATH, '//*[@id="unit1"]/a')
+                    timezone.click()
+
+                # For barbecue, select time zone 11~16 or 17~22
+                usrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[5]/h5/span/label/span')
+                usrchkbox.click()
+
+                user_cnt = self.driver.find_element(By.XPATH, '//*[@id="user_cnt_area"]/div/div[1]/div/div/div/button[2]')
+                user_cnt.send_keys(Keys.ENTER)
+                usrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[5]/h5/span/label/span')
+                usrchkbox.click()
+                agrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[6]/h5/span/label/span')
+                agrchkbox.click()
+                deal_btn = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[3]/div/div[3]/button')
+                deal_btn.click()
+
+                # finalize the reservation
+                Alert(self.driver).accept()
+            except:
+                print('Not successful... trying again')
+                time.sleep(10)
             else:
-                index = 2
-            select_num_nite.select_by_index(index=index)
-
-            user_cnt = self.driver.find_element(By.XPATH, '//*[@id="user_cnt_area"]/div/div[1]/div/div/div/button[2]')
-            user_cnt.send_keys(Keys.ENTER)
-            usrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[5]/h5/span/label/span')
-            usrchkbox.click()
-            agrchkbox = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[2]/div[6]/h5/span/label/span')
-            agrchkbox.click()
-            deal_btn = self.driver.find_element(By.XPATH, '//*[@id="aform"]/div[3]/div[3]/div/div[3]/button')
-            deal_btn.click()
-
-            # finalize the reservation
-            Alert(self.driver).accept()
-        except:
-            print('error')
+                print('Successful!!!')
+                break
 
         if (c := input()) == 'q':
             pass
-
         self.driver.quit()
 
 
 if __name__ == '__main__':
     urls = {
         '10_Dtype': 'https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S210913093907438559',
+        '10_barbecue': 'https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S210913095611524537',
         '11_deck': 'https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S211014143810532855',
         '11_Dtype': 'https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S211014144304124526',
         '11_barbecue': 'https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S211014151656734620',
     }
-    # NanjiCamping(url=urls['10_Dtype'], date='1020').start()
+    NanjiCamping(url=urls['10_Dtype'], date='1023').start()
+
+    # Barbecue
+    # NanjiCamping(url=urls['10_barbecue'], date='1024', type='barbecue11').start()
 
     # Friday
     NanjiCamping(url=urls['11_Dtype'], date='1105').start()
-    # NanjiCamping(url=urls['11_deck'], date='1105').start()
+    NanjiCamping(url=urls['11_deck'], date='1105').start()
     # NanjiCamping(url=urls['11_Dtype'], date='1112').start()
     # NanjiCamping(url=urls['11_deck'], date='1112').start()
     # NanjiCamping(url=urls['11_Dtype'], date='1119').start()
