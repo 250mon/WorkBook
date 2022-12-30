f509ea878799175dc6be2644cf0fd287394cb5f6
Jyeyoung Song <lambki@naver.com>
2022-12-30 11:16:25 +0900

    .gitignore modified to include assets dir

--- QTtest/3_06_menubar.py
+++ QTtest/3_06_menubar.py
@@ -9,26 +9,4 @@ class MyApp(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        exitAction = QAction(QIcon('exit.png'), 'Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        # when hovering over the menu, the following message shows up in status bar
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.quit)
-
-        self.statusBar()
-
-        menubar = self.menuBar()
-        # for MacOS, native menubar turned off and menubar works like windows
-        menubar.setNativeMenuBar(False)
-        # &File enables a shortcut 'Alt+F'
-        filemenu = menubar.addMenu('&File')
-        filemenu.addAction(exitAction)
-
-        self.setWindowTitle('Menubar')
-        self.setGeometry(300, 300, 300, 300)
-        self.show()
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyApp()
-    sys.exit(app.exec_())
+        
