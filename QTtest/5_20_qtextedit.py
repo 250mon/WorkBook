9c2f160a54996e839a7da1e28c0e12210e570e11
Jye Young Song <lambki@naver.com>
2023-01-03 07:52:31 +0900

    upto 5_21_qtablewidget.py

--- QTtest/5_20_qtextedit.py
+++ QTtest/5_20_qtextedit.py
@@ -16,6 +16,23 @@ class MyApp(QWidget):
 
         self.te.textChanged.connect(self.text_changed)
 
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.lbl1)
+        vbox.addWidget(self.te)
+        vbox.addWidget(self.lbl2)
+        vbox.addStretch()
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('QTextEdit')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+    def text_changed(self):
+        text = self.te.toPlainText()
+        self.lbl2.setText('The number of words is ' +
+                          str(len(text.split())))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
