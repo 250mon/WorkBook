83d5de584abe7159dcd7bc8d164695d373aaf6da
Jye Young Song <lambki@naver.com>
2023-01-03 07:35:01 +0900

    upto 5_20_qtextedit.py

---
+++ QTtest/5_20_qtextedit.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QTextEdit, QVBoxLayout
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.lbl1 = QLabel('Enter your sentence:')
+        self.te = QTextEdit()
+        self.te.setAcceptRichText(False)
+        self.lbl2 = QLabel('The number of words is 0')
+
+        self.te.textChanged.connect(self.text_changed)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
