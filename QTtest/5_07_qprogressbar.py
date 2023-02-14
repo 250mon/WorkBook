25100d7673a89789f3a04b2421004a56d6d396c2
Jyeyoung Song <lambki@naver.com>
2023-02-14 19:22:46 +0900

    upto 5_08_qslider.py

--- QTtest/5_07_qprogressbar.py
+++ QTtest/5_07_qprogressbar.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QProgressBar
 from PyQt5.QtCore import QBasicTimer
 
 
-class MyApp(QWidget):
+class ProgressBar(QWidget):
 
     def __init__(self):
         super().__init__()
