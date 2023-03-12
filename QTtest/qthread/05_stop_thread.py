bb4404b3e22e258e1b7f61f193b2bd66268f0167
250mon <lambki@naver.com>
2023-03-12 09:13:56 +0900

    05, 06 updated

---
+++ QTtest/qthread/05_stop_thread.py
@@ -0,0 +1,60 @@
+import sys
+import time
+from PyQt5.QtCore import Qt, QObject, QThread, pyqtSignal
+from PyQt5.QtWidgets import (
+    QApplication,
+    QSpinBox,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+class frmMain(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.btStart = QPushButton('Start')
+        self.btStop = QPushButton('Stop')
+        self.counter = QSpinBox()
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.btStart)
+        self.layout.addWidget(self.btStop)
+        self.layout.addWidget(self.counter)
+        self.setLayout(self.layout)
+        self.btStart.clicked.connect(self.start_thread)
+        self.btStop.clicked.connect(self.stop_thread)
+
+    def stop_thread(self):
+        self.th.stop()
+
+    def loopfunction(self, x):
+        self.counter.setValue(x)
+
+    def start_thread(self):
+        self.th = thread(2)
+        self.th.loop.connect(self.loopfunction)
+        self.th.setTerminationEnabled(True)
+        self.th.start()
+
+class thread(QThread):
+    loop = pyqtSignal(int)
+
+    def __init__(self, x):
+        QThread.__init__(self)
+        self.x = x
+
+    def run(self):
+        for i in range(100):
+            self.x = i
+            self.loop.emit(self.x)
+            print(i)
+            time.sleep(1)
+
+    def stop(self):
+        self.terminate()
+
+
+app = QApplication(sys.argv)
+win = frmMain()
+
+win.show()
+sys.exit(app.exec_())
