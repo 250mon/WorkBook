1e2ea49968fe53f3d3d69250840a28236920ec4a
250mon <lambki@naver.com>
2023-05-04 08:43:49 +0900

    Working on db_mgr.py

--- QTtest/sjy/db_mgr.py
+++ QTtest/sjy/db_mgr.py
@@ -8,6 +8,8 @@ class DbMgr:
         self.file_path = file_path
         self.conn = None
         self.cursor = None
+        self.create_conn()
+        self.create_table()
 
     def create_conn(self):
         try:
@@ -18,7 +20,7 @@ class DbMgr:
 
     def create_table(self):
         sql_create_patient_table = """
-            CREATE TABLE IN NOT EXISTS patients (
+            CREATE TABLE IF NOT EXISTS patients (
                 id integer PRIMARY KEY,
                 hospital_id integer NOT NULL,
                 name text,
@@ -27,36 +29,43 @@ class DbMgr:
         """
 
         sql_create_gender_table = """
-            CREATE TABLE IN NOT EXISTS gender (
+            CREATE TABLE IF NOT EXISTS gender (
                 id integer PRIMARY KEY,
-                gender_name text
+                gender_name text,
+                UNIQUE (gender_name)
             )
         """
 
         sql_create_patient_time_table = """
-            CREATE TABLE IN NOT EXISTS patient_time (
+            CREATE TABLE IF NOT EXISTS patient_time (
                 id integer PRIMARY KEY,
                 patient_id integer NOT NULL,
                 gender_id integer NOT NULL,
                 consult_time integer,
                 date text,
-                recording_file text
-                FOREIGN KEY (gender_id) REFERENCES gender (id),
-                FOREIGN KEY (patient_id) REFERENCES patients (id)
+                recording_file text,
+                FOREIGN KEY (patient_id) REFERENCES patients(id),
+                FOREIGN KEY (gender_id) REFERENCES gender(id)
             )
         """
 
         try:
+            self.cursor.execute(sql_create_patient_table)
+            self.cursor.execute(sql_create_gender_table)
             self.cursor.execute(sql_create_patient_time_table)
         except Error as e:
             logging.error(e)
 
     def insert_gender(self):
-        sql = ''' INSERT INTO gender (gender_name) VALUES(?,?,?) '''
-        for g in ["M", "F"]:
-            self.cursor.execute(sql, g)
-        self.cursor.commit()
+        try:
+            sql = ''' INSERT INTO gender (gender_name) VALUES(?) '''
+            for g in ["M", "F"]:
+                self.cursor.execute(sql, g)
+            self.conn.commit()
+        except Error as e:
+            logging.error(e)
 
 
 if __name__ == "__main__":
-    db_mgr = DbMgr()
+    db_mgr = DbMgr('clinic.db')
+    db_mgr.insert_gender()
