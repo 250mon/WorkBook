fe2bad7f954c74e466d74080c8ab776ddcd86426
250mon <lambki@naver.com>
2023-06-19 12:36:37 +0900

    grid-layout test

--- QTtest/4_03_grid_layout.py
+++ QTtest/4_03_grid_layout.py
@@ -14,16 +14,28 @@ class MyApp(QWidget):
         grid = QGridLayout()
         self.setLayout(grid)
 
-        grid.addWidget(QLabel('Title:'), 0, 0)
-        grid.addWidget(QLabel('Author'), 1, 0)
-        grid.addWidget(QLabel('Review:'), 2, 0)
+        grid.addWidget(QLabel('Title:'),  0, 0, 1, 1)
+        grid.addWidget(QLabel('Author:'), 1, 0, 1, 1)
+        grid.addWidget(QLabel('Review:'), 2, 0, 1, 1)
 
-        grid.addWidget(QLineEdit(), 0, 1)
-        grid.addWidget(QLineEdit(), 1, 1)
-        grid.addWidget(QTextEdit(), 2, 1)
+        grid.addWidget(QLineEdit(), 0, 1, 1, 1)
+        grid.addWidget(QLineEdit(), 1, 1, 1, 1)
+        grid.addWidget(QTextEdit(), 2, 1, 1, 1)
+
+        # grid.setColumnMinimumWidth(0, 20)
+        # grid.setColumnMinimumWidth(1, 20)
+        grid.setColumnStretch(0, 5)
+        grid.setColumnStretch(1, 10)
+
+        # grid.setRowMinimumHeight(0, 20)
+        # grid.setRowMinimumHeight(1, 20)
+        # grid.setRowMinimumHeight(2, 10)
+        grid.setRowStretch(0, 10)
+        grid.setRowStretch(1, 5)
+        grid.setRowStretch(2, 5)
 
         self.setWindowTitle('QGridLayout')
-        self.setGeometry(300, 300, 300, 200)
+        # self.setGeometry(300, 300, 500, 200)
         self.show()
 
 
