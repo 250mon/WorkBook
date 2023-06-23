247fc392dd2f35fe918e0df0b89b5df2bb64ec16
Jye Young Song <lambki@naver.com>
2023-06-24 07:41:22 +0900

    .

--- QTtest/model_view/item_table_view_model_mapper/textedit_delegate.py
+++ QTtest/model_view/item_table_view_model_mapper/textedit_delegate.py
@@ -1,11 +1,30 @@
-from PySide6.QtWidgets import QWidget, QStyledItemDelegate,\
-    QStyleOptionViewItem, QTextEdit
-from PySide6.QtCore import QModelIndex, Qt, QAbstractItemModel
+from typing import Union
+from PySide6.QtWidgets import (
+    QWidget, QStyledItemDelegate, QStyleOptionViewItem,
+    QTextEdit
+)
+from PySide6.QtCore import (
+    QModelIndex, Qt, QAbstractItemModel, QPersistentModelIndex,
+    QSize
+)
+from PySide6.QtWidgets import QStyledItemDelegate, QStyle
 
 
 class TextEditDelegate(QStyledItemDelegate):
     def __init__(self, parent=None):
         super().__init__(parent)
+
+    def paint(self, painter, option, index):
+       if index.column() == 1:
+           QStyledItemDelegate.paint(self, painter, option, index)
+       else:
+           QStyledItemDelegate.paint(self, painter, option, index)
+
+    def sizeHint(self,
+                 option: QStyleOptionViewItem,
+                 index: Union[QModelIndex, QPersistentModelIndex]) -> QSize:
+        return QStyledItemDelegate.sizeHint(self, option, index)
+
 
     def createEditor(self,
                      parent: QWidget,
