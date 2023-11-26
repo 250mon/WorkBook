e651843433e7c1e2dbc2d5a25987b82fd80ed631
Jye Young Song <lambki@naver.com>
2023-11-26 20:16:58 +0900

    system error logging

--- Logging/logging_in_subclasses/logger_my_v1/common/log_util.py
+++ Logging/logging_in_subclasses/logger_my_v1/common/log_util.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import logging
 import logging.config
 import yaml
@@ -6,10 +8,18 @@ from common.singleton import Singleton
 
 class Logs(metaclass=Singleton):
     def __init__(self):
+        os.makedirs('./log', exist_ok=True)
         with open('common/log_config.yaml', 'rt') as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
             logging.config.dictConfig(config)
             print("logging config applied")
 
+        self.err_logger = logging.getLogger("cl")
+        sys.excepthook = self.handle_exception
+
     def get_logger(self, name: str) -> logging.Logger:
         return logging.getLogger(name)
+
+    def handle_exception(self, exc_type, exc_value, exc_traceback):
+        self.err_logger.error("Unexpected exception",
+                              exc_info=(exc_type, exc_value, exc_traceback))
--- Logging/logging_in_subclasses/logger_my_v1/main.py
+++ Logging/logging_in_subclasses/logger_my_v1/main.py
@@ -1,13 +1,23 @@
 from global_logger.cc import CC
 from class_logger.qq import QQ
+from common.log_util import Logs
 
 
 def main():
+    logger = Logs().get_logger('cl')
+
     cc = CC()
     cc.run_cc()
 
     qq = QQ()
     qq.run_qq()
+
+    try:
+        a = 1/0
+        print(a)
+    except Exception as e:
+        logger.debug("division error %s", e)
+
 
 
 if __name__ == '__main__':
